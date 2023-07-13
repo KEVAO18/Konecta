30364bec7d4059e2f7548f178d24fb16fa180d11
KEVAO18 <kevin100orrego@gmail.com>
2023-07-13 01:49:49 -0500

    proyecto terminado

--- README.md
+++ README.md
@@ -1,8 +1,46 @@
 # Konecta
 
+  
+
 ## Instalacion
+
 - PHP 8.2.4 o superior
-- MySQL/MariaDB
+
+- MySQL/MariaDB 5.2.1 o superior
+
+- Composer
+
+  
+
+### Paso a paso:
+cambiar el valor de `PAGE_SERVE` por la ubicacion del fichero en el que se encuentra el sistema y configurar los datos para conectarse al servidor mysql
+
+Ejecutar el comando 
+```bash 
+composer require vlucas/phpdotenv 
+```
+ para poder hacer uso del archovo .env y ejecutar el comando:
+```bash 
+ composer require twbs/bootstrap:5.3.0
+``` 
+para poder usar la version 5.3 de bootstrap.
+
+Ahora proceda crear la base de datos 
+```sql 
+CREATE DATABASE konectadb;
+```
+e importar la base de datos incluida en el fichero raiz
+
 ## Consultas
-- SELECT productos.Nombre FROM `productos` ORDER BY productos.Stock DESC LIMIT 1;
-- SELECT producto_id, SUM(unidades) AS unidadesTotales FROM ventas GROUP BY ventas.producto_id ORDER BY unidadesTotales DESC LIMIT 1;
+
+```sql 
+SELECT productos.Nombre FROM `productos` ORDER BY productos.Stock DESC LIMIT 1;
+```
+
+```sql 
+SELECT producto_id, SUM(unidades) AS unidadesTotales FROM ventas GROUP BY ventas.producto_id ORDER BY unidadesTotales DESC LIMIT 1;
+```
+
+## Notas
+- [Hoja de vida actualizada](http://kevaodv.tech/assets/docs/CV1.pdf)
+- [Portfolio](http://kevaodv.tech/home)
---
+++ konectadb.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 13-07-2023 a las 08:21:54
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `konectadb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `ID` int(11) NOT NULL,
+  `Nombre` varchar(50) NOT NULL,
+  `Referencia` varchar(50) NOT NULL,
+  `Precio` int(9) NOT NULL,
+  `Peso` int(9) NOT NULL,
+  `Categoria` varchar(50) NOT NULL,
+  `Stock` int(11) NOT NULL,
+  `FechaCreacion` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `ventas`
+--
+
+CREATE TABLE `ventas` (
+  `id` int(11) NOT NULL,
+  `producto_id` int(11) NOT NULL,
+  `unidades` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- Indices de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `producto_id` (`producto_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `ventas`
+--
+ALTER TABLE `ventas`
+  ADD CONSTRAINT `ventas_ibfk_1` FOREIGN KEY (`producto_id`) REFERENCES `productos` (`ID`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
